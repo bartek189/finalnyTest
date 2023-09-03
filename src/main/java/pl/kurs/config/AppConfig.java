11abeb3577aaf95d75ea699647d2bb02e6b8f81a
bartekk189 <bartekk189@outlook.com>
2023-09-03 20:20:43 +0200

    sql init

--- src/main/java/pl/kurs/config/AppConfig.java
+++ src/main/java/pl/kurs/config/AppConfig.java
@@ -1,34 +1,15 @@
 package pl.kurs.config;
 
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-//import springfox.documentation.builders.ParameterBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.schema.ModelRef;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Collections;
-
-//@EnableSwagger2
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
@@ -48,17 +29,5 @@ public class AppConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-
-
-    private final DataSource dataSource;
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void init() {
-        try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute("CREATE VIEW  SHAPEWITHPARAMETERS AS SELECT DTYPE, ID, CREATED_AT,CREATED_BY,LAST_MODIFIED_AT,LAST_MODIFIED_BY,TYPE,VERSION,SIDE,WIDTH,HEIGHT,RADIUS,CASE WHEN TYPE = 'CIRCLE' THEN  PI() * RADIUS * RADIUS WHEN TYPE = 'RECTANGLE' THEN WIDTH * HEIGHT When TYPE = 'SQUARE' THEN SIDE*SIDE ELSE NULL END AS area, CASE WHEN TYPE = 'CIRCLE' THEN 2 * PI() * RADIUS WHEN TYPE = 'RECTANGLE' THEN 2 * (WIDTH + HEIGHT) WHEN TYPE = 'SQUARE' THEN 4 * SIDE ELSE NULL END AS perimeter FROM SHAPE");
-        } catch (SQLException e) {
-        }
-    }
 
 }
--- src/main/java/pl/kurs/util/SecurityUtil.java
+++ src/main/java/pl/kurs/util/SecurityUtil.java
@@ -3,6 +3,7 @@ package pl.kurs.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import pl.kurs.entity.model.ERole;
 import pl.kurs.entity.model.User;
 import pl.kurs.repository.UserRepository;
 
@@ -14,6 +15,6 @@ public class SecurityUtil {
 
     public User getUser() {
         String userName = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return userRepository.findByUserName(userName).orElseThrow();
+        return userRepository.findByUserNameWithRoles(userName).orElseThrow();
     }
 }
